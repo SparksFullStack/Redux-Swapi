0de471f0cf693cc09e2ced4689f54be7a7a2b466
SparksFullStack <csparks@antioch.edu>
2018-07-11 23:47:03 -0700

    made some changes to optimize logger

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -9,10 +9,8 @@ const initialState = {
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     case "FETCHING":
-      console.log("Fetching State");
       return { ...state, fetching: true };
     case "FETCHED":
-      console.log("The state was fetched!", action.payload)
       return {
         ...state,
         chars: action.payload,
@@ -20,7 +18,6 @@ export const charsReducer = (state = initialState, action) => {
         fetched: true,
       }
     case "ERROR":
-      console.log("There was an error")
       return { ...state, fetching: false, error: action.payload}
     default:
       return state;
